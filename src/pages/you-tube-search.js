8ebe6d71036da59eb5c50557d612f7ceda350b59
MeotJH <kimjh0417@gsitm.com>
2023-08-02 13:53:31 +0900

    card.js file make use to re-use item ( components ) ;

--- src/pages/you-tube-search.js
+++ src/pages/you-tube-search.js
@@ -2,11 +2,31 @@ import Card from "../components/card";
 import React from "react";
 
 function YouTubeSearch() {
+  const [popularList, setPopularList] = React.useState([]);
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const tempD = await googlePopularVideo();
+      setPopularList(tempD.items);
+    }
+
+    fetchData();
+  }, []);
+
+  const googlePopularVideo = async () => {
+    const API_KEY = process.env.REACT_APP_GOOGLE_KEY;
+    const tempPopular = await fetch(
+      `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=10&regionCode=kr&key=${API_KEY}`
+    );
+    const afterAwait = await tempPopular.json();
+    return afterAwait;
+  };
   return (
     <section className=" min-h-screen flex items-center">
       <Card
-        title="이것은 제목입니다"
-        description="이것은 내용입니다이것은 내용입니다이것은 내용입니다이것은 내용입니다이것은 내용입니다이것은 내용입니다이것은 내용입니다이것은 내용입니다이것은 내용입니다이것은 내용입니다이것은 내용입니다"
+        popularList={popularList}
+        title="YOU TUBE 인급동"
+        hrefUrl="https://www.youtube.com/watch?v="
       />
     </section>
   );
