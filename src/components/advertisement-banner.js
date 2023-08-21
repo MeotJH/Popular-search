64eb702d8435ba368ef1a9adf19b8c812deb9004
박기철 <gcpark@gsitm.com>
2023-08-21 13:16:56 +0900

    광고적용(카카오애드핏)

--- src/components/advertisement-banner.js
+++ src/components/advertisement-banner.js
@@ -3,7 +3,8 @@ import KakaoAdFit from "./kakao-ad-fit";
 const AdvertisementBanner = () => {
   return (
     <>
-      <div className="bg-slate-500 h-60">광고배너들어갈자리</div>
+      {/* <div className="bg-slate-500 h-60">광고배너들어갈자리</div> */}
+      <KakaoAdFit></KakaoAdFit>
     </>
   );
 };
