ad9f594d5e6a6a2eff93aa736f54768fe5d809a8
MeotJH <kimjh0417@gsitm.com>
2023-08-21 10:03:34 +0900

    배너 컴포넌트 만들어서 유튜브 화면에 적용

---
+++ src/components/advertisement-banner.js
@@ -0,0 +1,9 @@
+const AdvertisementBanner = () => {
+  return (
+    <>
+      <div className="bg-slate-500 h-60">광고배너들어갈자리</div>
+    </>
+  );
+};
+
+export default AdvertisementBanner;
--- src/components/card-item-you-tube.js
+++ src/components/card-item-you-tube.js
@@ -1,25 +1,36 @@
 import React from "react";
+import AdvertisementBanner from "./advertisement-banner";
 
 const CardItemYouTube = ({ popularList, hrefUrl }) => {
+  const ADBanner = ({ index }) => {
+    const bannerIndex = popularList.length / 2 - 1;
+    if (bannerIndex === index) {
+      return <AdvertisementBanner />;
+    }
+  };
+
   return (
     <>
       {popularList.map((item, index) => {
         return (
-          <div key={index} className="border-y-8 border-bodyColor">
-            <a
-              href={`${hrefUrl}${item.id}`}
-              target="_blank"
-              rel="noreferrer"
-              className="border-y-8 border-bodyColor"
-            >
-              <h2 className="text-1xl font-extrabold">
-                {item.snippet.title}
-              </h2>
-              <img
-                src={item.snippet.thumbnails.standard.url}
-                alt={item.id}
-              ></img>
-            </a>
+          <div key={index}>
+            <ADBanner index={index} />
+            <div className="border-y-8 border-bodyColor">
+              <a
+                href={`${hrefUrl}${item.id}`}
+                target="_blank"
+                rel="noreferrer"
+                className="border-y-8 border-bodyColor"
+              >
+                <h2 className="text-1xl font-extrabold dark:text-white">
+                  {item.snippet.title}
+                </h2>
+                <img
+                  src={item.snippet.thumbnails.standard.url}
+                  alt={item.id}
+                ></img>
+              </a>
+            </div>
           </div>
         );
       })}
