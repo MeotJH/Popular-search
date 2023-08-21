a0c3aacbc2b71a93c7fc7355157819b5a01dcd06
MeotJH <kimjh0417@gsitm.com>
2023-08-21 13:35:35 +0900

    Revert "구글 태그 ( 구글 아날리틱스 ) 추가"
    This reverts commit 6bf0520cb1cf6db67ece246518a7c21419c08972.

--- src/components/advertisement-banner.js
+++ src/components/advertisement-banner.js
@@ -3,16 +3,7 @@ import KakaoAdFit from "./kakao-ad-fit";
 const AdvertisementBanner = () => {
   return (
     <>
-      <div className="h-70 flex justify-center">
-        <iframe
-          src="https://ads-partners.coupang.com/widgets.html?id=699559&template=carousel&trackingCode=AF8838907&subId=&width=500&height=250&tsource="
-          width="500"
-          height="250"
-          frameborder="0"
-          scrolling="no"
-          referrerpolicy="unsafe-url"
-        ></iframe>
-      </div>
+      <div className="bg-slate-500 h-60">광고배너들어갈자리</div>
     </>
   );
 };
